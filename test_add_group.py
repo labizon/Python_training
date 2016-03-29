c7585b8733f3335538984292f90828d69d67a842
labizon <lyabizon@hotmail.com>
2016-03-29 09:00:24 -0700

    Выделена fixture и осуществлен переход на py.test

--- test_add_group.py
+++ test_add_group.py
@@ -1,76 +1,25 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from group import Group
+from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-    
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
+@pytest.fixture
+def app(request):
+    fixture = Application() # -- Инициализация.  Создание fixture --
+    request.addfinalizer(fixture.destroy) # -- Указание на то, как эта fixture должна быть разрушена  --
+    return fixture
 
-    def return_to_groups_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd, group):
-        self.open_groups_page(wd)
-        # -- New Group Creation --
-        wd.find_element_by_name("new").click()
-        # -- Fill Group Form --
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # -- Submit Group Creation --
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page(wd)
+def test_add_group(app):  # -- Тестовый метод, принимающий fixture в качестве параметра и вызывающий
+                          # -- в ней вспомагательные методы  --
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="Test", header="test", footer="test"))
+    app.logout()
 
-    def open_groups_page(self, wd):
-         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="Test", header="test", footer="test"))
-        self.return_to_groups_page(wd)
-        self.logout(wd)
-
-    def test_add_empty_group(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.logout(wd)
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add_empty_group(app):  # -- Тестовый метод, принимающий fixture в качестве параметра и вызывающий
+                                # -- в ней вспомагательные методы  --
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
