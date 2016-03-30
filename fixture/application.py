a1475341ade6a30647445b74f2e55cacd72ccb97
labizon <lyabizon@hotmail.com>
2016-03-30 15:08:16 -0700

    optimization

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ class Application:  # --  Класс, содержащий все вспомаг
 
     def __init__(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
 
